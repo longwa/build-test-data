ef9747470f108376fe4b512ef5ceea537b7e722a
Aaron Long <along@triu.com>
2013-04-20 00:49:41 -0400

    Change version to 2.0.5

--- build-test-data/BuildTestDataGrailsPlugin.groovy
+++ build-test-data/BuildTestDataGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import groovy.util.logging.Commons
 @Commons
 class BuildTestDataGrailsPlugin {
     // the plugin version
-    def version = "2.0.5-RC3"
+    def version = "2.0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0 > *"
     def dependsOn = [:]
