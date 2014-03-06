bc3028c4a351cbcd71bc3a96d6245d91706c423b
Ted Naleid <contact@naleid.com>
2014-03-05 21:36:24 -0600

    bump version number for release

--- build-test-data/BuildTestDataGrailsPlugin.groovy
+++ build-test-data/BuildTestDataGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import groovy.util.logging.Commons
 
 @Commons
 class BuildTestDataGrailsPlugin {
-    def version = "2.1.1-SNAPSHOT"
+    def version = "2.1.2"
     def grailsVersion = "2.0.0 > *"
     def loadAfter = ['services', 'dataSource', 'hibernate', 'hibernate4', 'validation']
     def watchedResources = ["file:./grails-app/domain/**.groovy"]
