8949277186dcc07aeeeef7467f3a133a5c610fe5
Ted Naleid <contact@naleid.com>
2015-01-15 20:28:56 -0600

    update to hibernate 4 cache reference

--- bookStore/grails-app/conf/DataSource.groovy
+++ bookStore/grails-app/conf/DataSource.groovy
@@ -7,7 +7,7 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
 // environment specific settings
 environments {
