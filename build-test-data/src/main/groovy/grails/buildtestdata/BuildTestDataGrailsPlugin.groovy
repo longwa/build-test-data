f3144194d869d9aa818611c32d29b4525b74a423
Aaron Long <along@triu.com>
2017-11-27 10:57:11 -0500

    Move to Grails 3.3.2 and Gradle 3.5

--- build-test-data/src/main/groovy/grails/buildtestdata/BuildTestDataGrailsPlugin.groovy
+++ build-test-data/src/main/groovy/grails/buildtestdata/BuildTestDataGrailsPlugin.groovy
@@ -1,63 +1,27 @@
 package grails.buildtestdata
 
-import groovy.util.logging.Commons
+import grails.plugins.Plugin
+import groovy.transform.CompileStatic
 
-@Commons
+@CompileStatic
 @SuppressWarnings("GroovyUnusedDeclaration")
-class BuildTestDataGrailsPlugin {
-    def grailsVersion = "3.0.0 > *"
-    def loadAfter = ['services', 'dataSource', 'hibernate', 'hibernate4', 'validation']
-    def watchedResources = ["file:./grails-app/domain/**.groovy"]
+class BuildTestDataGrailsPlugin extends Plugin {
+    def grailsVersion = "3.3.0 > *"
+
     def title = "Build Test Data Plugin"
     def description = 'Enables the easy creation of test data by automatically satisfying most constraints.'
     def license = "APACHE"
+    def documentation = "https://github.com/longwa/build-test-data/wiki"
 
     def developers = [
         [name: "Aaron Long", email: "aaron@aaronlong.me"],
         [name: "Ted Naleid", email: "contact@naleid.com"],
-        [name: "Søren Berg Glasius", email: "soeren@glasius.dk"],
-        [name: "Joe Hoover"],
-        [name: "Matt Sheehan"],
-        [name: "Gregor Petrin"],
-        [name: "Michael Cameron"],
-        [name: "Jaime Garcia"]
     ]
 
-    def documentation = "https://github.com/longwa/build-test-data/wiki"
     def issueManagement = [system: 'github', url: 'https://github.com/longwa/build-test-data/issues']
     def scm = [url: 'https://github.com/longwa/build-test-data/']
 
-    def doWithApplicationContext = { appCtx ->
-        DomainUtil.setApplication(appCtx.grailsApplication)
-        decorateDomainClasses(appCtx.grailsApplication.domainClasses)
-    }
-
-    def onChange = { event ->
-        decorateDomainClasses(grailsApplication.domainClasses)
-    }
-
-    private decorateDomainClasses(domainClasses) {
-        def buildTestDataService = new BuildTestDataService()
-
-        log.debug("Loading config file (if present)")
-        TestDataConfigurationHolder.loadTestDataConfig()
-
-        if (!TestDataConfigurationHolder.isPluginEnabled()) {
-            log.warn("build-test-data plugin is disabled in this environment")
-        }
-
-        log.debug('build-test-data plugin enabled, decorating domain classes with build methods')
-
-        domainClasses.each { domainClass ->
-            log.debug("decorating $domainClass with build-test-data 'build' methods")
-            try {
-                buildTestDataService.decorateWithMethods(domainClass)
-            }
-            catch (Exception e) {
-                log.error "Error decorating ${domainClass}. Message: [${e.getMessage()}]"
-            }
-        }
-
-        log.debug("done decorating domain classes with 'build' methods")
+    void doWithApplicationContext() {
+        DomainUtil.setApplication(grailsApplication)
     }
 }
