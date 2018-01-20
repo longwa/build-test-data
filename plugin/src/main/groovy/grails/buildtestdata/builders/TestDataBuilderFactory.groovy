76a58c8ae0c46ee9e407d9150a646120b034aca3
Joshua B <joshdev@9ci.com>
2018-01-20 05:33:05 -0600

    deletes

--- plugin/src/main/groovy/grails/buildtestdata/builders/TestDataBuilderFactory.groovy
+++
@@ -1,6 +0,0 @@
-package grails.buildtestdata.builders
-
-interface TestDataBuilderFactory<T extends DataBuilder> extends Comparable<TestDataBuilderFactory> {
-    T build(Class target)
-    boolean supports(Class clazz)
-}
